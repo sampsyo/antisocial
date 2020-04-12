311fedff24ca7733705fc5eed26eef53a21c4428
Adrian Sampson <adrian@radbox.org>
2020-04-12 11:08:02 -0400

    Refactor away unpleasantness

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,34 @@
-use warp::Filter;
 use serde::Deserialize;
 use std::fs;
 use std::sync::Arc;
+use warp::Filter;
 
 #[derive(Deserialize)]
 struct Config {
-    url: String
+    url: String,
 }
 
-fn routes(config: &Arc<Config>) -> impl Filter<Extract = impl warp::Reply,
-                              Error = warp::Rejection> + Clone {
-    let c = config.clone();
-    warp::path!("hello" / String)
-        .map(move |name| format!("Hello from {}, {}!", (*c).url, name))
+fn loadcfg() -> Config {
+    let configstr = fs::read_to_string("config.toml").unwrap();
+    toml::from_str(&configstr).unwrap()
+}
+
+async fn say_hello(
+    name: String,
+    config: Arc<Config>,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    let reply = format!("Hello from {}, {}!", (*config).url, name);
+    Ok(warp::reply::html(reply))
 }
 
 #[tokio::main]
 async fn main() {
-    let configstr = fs::read_to_string("config.toml").unwrap();
-    let config: Arc<Config> = Arc::new(toml::from_str(&configstr).unwrap());
+    let config: Arc<Config> = Arc::new(loadcfg());
+    let config_filt = warp::any().map(move || Arc::clone(&config));
 
-    let hello = routes(&config);
+    let hello = warp::path!("hello" / String)
+        .and(config_filt)
+        .and_then(say_hello);
 
     warp::serve(hello)
         .run(([127, 0, 0, 1], 3030))
